d1553a0218102f409e38a04813dc4b5926075224
Murilo Andrade <luizmurilo.97@gmail.com>
2022-03-13 03:04:53 -0300

    Pronto para deploy

--- library/settings.py
+++ library/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-7r=di4+vmwe0t=-yw@pwg(qx2$^4u3ag_ub)wtgsz16iy7k96n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://mrolibrary.herokuapp.com/']
 
 
 # Application definition
@@ -127,3 +128,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
